6c7820d912c840f0a34c355c6f05033039df0900
Finn Roberts <finn.a.roberts@gmail.com>
2025-09-25 16:58:20 +0200

    Add unit tests
    Uses vdiffr for plot comparisons; refactors rFunction.R to enable testing of intermediate output

--- RFunction.R
+++ RFunction.R
@@ -22,23 +22,70 @@ rFunction <- function(data,
                       caption = "",
                       file_format = "mp4",
                       margin_factor = 1.1,
-                      equidistant = FALSE) {
+                      equidistant = FALSE,
+                      verbose = !testthat::is_testing()) {
   # Copy data so we can return a non-modified version
   data_orig <- data
   
+  frames <- generate_frames(
+    data = data,
+    res = res,
+    unit = unit,
+    map_type = map_type,
+    map_token = map_token,
+    map_res = map_res,
+    col_opt = col_opt,
+    path_pal = path_pal,
+    colour_paths_by = colour_paths_by,
+    path_legend = path_legend,
+    caption = caption,
+    margin_factor = margin_factor,
+    equidistant = equidistant,
+    verbose = verbose
+  )
+  
+  # Allow for unit testing on frames output
+  if (testthat::is_testing()) {
+    out_file <- file.path(tempdir(), paste0("animation_moveVis.", file_format))
+  } else {
+    out_file <- appArtifactPath(paste0("animation_moveVis.", file_format))
+  }
+  
+  # animate frames
+  animate_frames(
+    frames, 
+    out_file = out_file, 
+    overwrite = TRUE, 
+    fps = fps,
+    display = FALSE,
+    verbose = verbose
+  )
+  
+  data_orig
+}
+
+group_data <- function(x) {
   # Ensure move2 object is ordered correctly before aligning
-  if(!mt_is_track_id_cleaved(data)){
+  if(!mt_is_track_id_cleaved(x)){
     logger.info("Regrouping data by individual/track.")
-    data <- dplyr::arrange(data, mt_track_id(data))
+    x <- dplyr::arrange(x, mt_track_id(x))
   }
   
-  if(!mt_is_time_ordered(data)){
+  x
+}
+
+time_order_data <- function(x) {
+  if(!mt_is_time_ordered(x)){
     logger.info("Ordering track data chronologically.")
-    data <- dplyr::arrange(data, mt_track_id(data), mt_time(data))
+    x <- dplyr::arrange(x, mt_track_id(x), mt_time(x))
   }
   
-  if (!mt_has_unique_location_time_records(data)){
-    n_dupl <- length(which(duplicated(paste(mt_track_id(data), mt_time(data)))))
+  x
+}
+
+deduplicate <- function(x) {
+  if (!mt_has_unique_location_time_records(x)){
+    n_dupl <- length(which(duplicated(paste(mt_track_id(x), mt_time(x)))))
     
     logger.info(
       paste0(
@@ -48,16 +95,21 @@ rFunction <- function(data,
     )
     
     # In case of duplicates, keep the entry with fewest missing values
-    data <- data %>%
-      mutate(n_na = rowSums(is.na(pick(everything())))) %>%
-      arrange(n_na) %>% # TODO: Does this not disrupt chronological ordering?
-      mt_filter_unique(criterion = "first")
+    x <- mutate(x, n_na = rowSums(is.na(pick(everything()))))
+    x <- arrange(x, "n_na") # TODO: Does this not disrupt chronological ordering?
+    x <- mt_filter_unique(x, criterion = "first")
   }
   
-  # Parse resolution info, which can either be a string or a number with units
-  # (which are specified with separate MoveApps inputs)
+  x
+}
+
+# Parse resolution info, which can either be a string or a number with units
+# (which are specified with separate MoveApps inputs)
+parse_resolution <- function(res, unit) {
+  res_options <- c("mean", "minimum", "maximum", "median")
+  
   res <- tryCatch({
-    res <- match.arg(res, c("mean", "minimum", "maximum", "median"))
+    res <- match.arg(res, res_options)
     logger.info(paste0("Using resolution: ", res))
     res
   },
@@ -75,8 +127,12 @@ rFunction <- function(data,
     res
   })
   
-  # Service and type are contained in a single setting and concatenated with `:`
-  # Extract each element here for use in `frames_spatial()`
+  res
+}
+
+# Service and type are contained in a single setting and concatenated with `:`
+# Extract each element here for use in `frames_spatial()`
+parse_map_spec <- function(map_type, map_token) {
   map_service <- sub(":.*$", "", map_type)
   map_type <- sub("^[^:]*:", "", map_type)
   
@@ -92,10 +148,36 @@ rFunction <- function(data,
         "Using OSM topographic basemap."
       )
     )
-  
+    
     map_service <- "osm"
     map_type <- "topographic"
   }
+  
+  list(map_service = map_service, map_type = map_type)
+}
+
+generate_frames <- function(data,
+                            res = "mean",
+                            unit = "hours",
+                            map_type = "osm:streets",
+                            map_token = "",
+                            map_res = 1,
+                            col_opt = "one",
+                            path_pal = "Set 2",
+                            colour_paths_by = "",
+                            path_legend = TRUE,
+                            caption = "",
+                            margin_factor = 1.1,
+                            equidistant = FALSE,
+                            verbose = !testthat::is_testing()) {
+  # Reorganize data as needed
+  data <- deduplicate(time_order_data(group_data(data)))
+  
+  # Interpret resolution/unit input
+  res <- parse_resolution(res, unit)
+  
+  # Split map provider from map type and check API access
+  map_spec <- parse_map_spec(map_type, map_token)
   
   if (map_res < 0 | map_res > 1) {
     logger.warn(
@@ -137,7 +219,7 @@ rFunction <- function(data,
     legend_title <- "Track IDs"
   }
   
-  m <- align_move(data, res = res)
+  m <- align_move(data, res = res, verbose = verbose)
   
   # Note: this is based on experimental moveVis version awaiting review and not
   # yet released to dev.
@@ -148,12 +230,13 @@ rFunction <- function(data,
     margin_factor = margin_factor,
     path_legend = path_legend,
     path_legend_title = legend_title,
-    map_service = map_service,
+    map_service = map_spec[["map_service"]],
     map_token = map_token,
-    map_type = map_type, 
+    map_type = map_spec[["map_type"]], 
     map_res = map_res,
     path_alpha = 0.5,
-    equidistant = equidistant
+    equidistant = equidistant,
+    verbose = verbose
   )
   
   frames <- frames |>
@@ -163,13 +246,5 @@ rFunction <- function(data,
     add_timestamps(type = "label") |>
     add_progress(colour = "white")
   
-  # animate frames
-  animate_frames(
-    frames, 
-    out_file = appArtifactPath(paste0("animation_moveVis.", file_format)),
-    overwrite = TRUE, 
-    fps = fps
-  )
-  
-  data_orig
+  frames
 }
--- tests/testthat/test_RFunction.R
+++ tests/testthat/test_RFunction.R
@@ -1,11 +1,95 @@
-test_data <- test_data("input3_move2loc_LatLon.rds")
+# Better way to handle imports?
+library(testthat)
+source(test_path("helper.R"))
+source("../../src/common/logger.R")
+source("../../RFunction.R")
 
-test_that("happy path", {
-  actual <- rFunction(data = test_data, sdk = "unit test", year = 1998)
-  expect_equal(unique(lubridate::year(mt_time(actual))), 1998)
+# Tests can be run in-session with
+# testthat::test_file(testthat::test_path("test_RFunction.R"))
+
+d <- test_data()
+
+test_that("Can animate frames with default values", {
+  capture.output(
+    rFunction(d)
+  )
+  expect_true("animation_moveVis.mp4" %in% list.files(tempdir()))
 })
 
-test_that("year not included", {
-  actual <- rFunction(data = test_data, sdk = "unit test", year = 1900)
-  expect_null(actual)
+test_that("Can color with single color", {
+  capture.output(
+    frames <- generate_frames(d, map_res = 0.1)
+  )
+  vdiffr::expect_doppelganger("frames-5-one", frames[[5]])
+})
+
+test_that("Can color by track ID", {
+  capture.output(
+    frames <- generate_frames(d, col_opt = "trackid", map_res = 0.1)
+  )
+  vdiffr::expect_doppelganger("frames-5-trackid", frames[[5]])
+  
+  capture.output(
+    frames <- generate_frames(d, col_opt = "trackid", path_pal = "Viridis", map_res = 0.1)
+  )
+  vdiffr::expect_doppelganger("frames-5-trackid-viridis", frames[[5]])
+})
+
+# Int handling is not yet in the latest moveVis dev package so this will fail
+test_that("Can color by attribute", {
+  skip("Not on moveVis GitHub yet")
+  capture.output(
+    frames <- generate_frames(
+      d,
+      col_opt = "other",
+      colour_paths_by = "tag_id",
+      path_pal = "Harmonic",
+      map_res = 0.1
+    )
+  )
+  vdiffr::expect_doppelganger("frames-5-tagid", frames[[5]])
+})
+
+test_that("Warn if no API token", {
+  withr::local_envvar(list(STADIA_API_KEY = NA))
+  
+  expect_output(
+    frames <- generate_frames(d, map_type = "osm_stadia:alidade_smooth"),
+    paste0(
+      "\\[WARN\\] Map service osm_stadia requires API authorization, ",
+      "but no key was provided.+"
+    )
+  )
+  expect_equal(frames$aesthetics$map_service, "osm")
+  expect_equal(frames$aesthetics$map_type, "topographic")
+})
+
+test_that("`margin_factor` works", {
+  capture.output(
+    frames <- generate_frames(d, margin_factor = 0.6, map_res = 0.1)
+  )
+  vdiffr::expect_doppelganger("frames-5-mf-low", frames[[5]])
+  
+  capture.output(
+    frames <- generate_frames(d, margin_factor = 1.5, map_res = 0.1)
+  )
+  vdiffr::expect_doppelganger("frames-5-mf-high", frames[[5]])
+})
+
+test_that("Can provide res as text or numeric", {
+  # Use output as proxy here, since it's difficult to programatically
+  # determine what alignment was used without returning the output of
+  # `align_move()`
+  expect_output(
+    frames <- generate_frames(d, res = "max"),
+    "\\[INFO\\] Using resolution: maximum"
+  )
+  expect_output(
+    frames <- generate_frames(d, res = 1, unit = "days"),
+    "\\[INFO\\] Using resolution: 1 \\(days\\)"
+  )
+  expect_output(
+    frames <- generate_frames(d, res = "foobar"),
+    "\\[WARN\\] Unrecognized resolution. Using resolution: mean"
+  )
 })
